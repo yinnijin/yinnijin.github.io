26dcef741db9b3fecaf3756b6ec90feca6f65d66
yinnijin <36608944+yinnijin@users.noreply.github.com>
2018-02-25 21:19:55 -0600

    Add files via upload
    task2

---
+++ comment.js
@@ -0,0 +1,10 @@
+
+function send() {
+	var x = document.getElementsByClassName("sendTerm");
+	 document.getElementById("new").innerHTML = x[0].value;
+document.getElementById("newtime").innerHTML = "02/26/2018 23:00";
+    var y = document.getElementById("newpost");
+    y.style.display = "block";
+    
+};
+
